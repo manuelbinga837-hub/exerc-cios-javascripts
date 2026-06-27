1a61654b0c9693db9b544a265255033f29893cb1
Macoder44 <manuelbinga837@gmail.com>
2026-06-28 00:05:58 +0100

    Create funções.js

---
+++ funções.js
@@ -0,0 +1,11 @@
+function quadrado(n){
+    return n*n
+}
+
+function somaQuadrados(n1,n2){
+    return quadrado(n1)+quadrado(n2)
+}
+let res=somaQuadrados(2,3)
+console.log(`Soma dos quadrados: ${res}`)
+
+
